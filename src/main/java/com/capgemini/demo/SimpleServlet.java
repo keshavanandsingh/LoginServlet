608162c04004ac68969a8ce7953ad19ce22812b7
keshavanandsingh <keshavanand22@gmail.com>
2020-11-26 20:48:56 +0530

    Added : My first tomcat servelet created

---
+++ src/main/java/com/capgemini/demo/SimpleServlet.java
@@ -0,0 +1,21 @@
+package com.capgemini.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class SimpleServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Hello World! How are you doing today!</h3>");
+		out.close();
+	}
+}
